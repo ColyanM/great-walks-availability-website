7a31968c020d2396e295606f9e6b483e68955e6d
ColyanM <colyanmiller@yahoo.com>
2026-09-18 19:59:54 +1200

    initial spring boot

---
+++ backend/src/main/java/com/example/greatwalkalerts/GreatWalkAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.example.greatwalkalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatWalkAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatWalkAlertsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/greatwalkalerts/GreatWalkAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greatwalkalerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatWalkAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
